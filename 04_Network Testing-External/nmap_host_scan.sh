31c1d7f84e7d552c5fb6107ed1779afdb37acf81
aeifkz <devilmaycry0616@hotmail.com>
2021-11-24 15:10:10 +0800

    fix Nmap and ad output directory

--- 04_Network Testing-External/nmap_host_scan.sh
+++ 04_Network Testing-External/nmap_host_scan.sh
@@ -3,7 +3,11 @@
 result=$(nmap -n -sn -PS22,80,443,445,3389 $1)
 
 #echo with "$variable" show the newline symbol
-host_list=$(echo "$result" | grep "map scan report for" | cut -d ' ' -f 5)
+host_list=$(echo "$result" | grep "Nmap scan report for" | cut -d ' ' -f 5)
+
+#create directory and output file
+mkdir -p nmap_output
+cd nmap_output
 
 for host in $host_list
 do
